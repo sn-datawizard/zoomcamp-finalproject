50aa028c1d7cb99dac2fefddfbc7d2d1c4a159c4
sn-datawizard <stefan.stohl@gmx.de>
2023-04-05 16:05:11 +0200

    Modify script to add comments

--- scripts/move_snowflake/load.py
+++ scripts/move_snowflake/load.py
@@ -39,10 +39,10 @@ df['Continent'].fillna('NONE', inplace=True)
 df['Region'].fillna('NONE', inplace=True)
 df['Population'].fillna(0.0, inplace=True)
 
-# Create pyspark session
-findspark.init('C:\spark-3.3.0-bin-hadoop3')
-os.environ['SPARK_HOME'] = 'C:\spark-3.3.0-bin-hadoop3'
-os.environ["JAVA_HOME"] = 'C:\Program Files\Java\jdk-18.0.1.1'
+# Create pyspark session (only for development in jupyter notebook)
+#findspark.init('C:\spark-3.3.0-bin-hadoop3')
+#os.environ['SPARK_HOME'] = 'C:\spark-3.3.0-bin-hadoop3'
+#os.environ["JAVA_HOME"] = 'C:\Program Files\Java\jdk-18.0.1.1'
 
 spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 
@@ -91,8 +91,6 @@ connection = helpers.connect_snowflake(username, password, account, warehouse, d
 helpers.write_snowflake(connection, df_transformed, table3)
 
 df['Year'] = pd.to_datetime(df['Year'].astype(str) + '-12-31', format='%Y-%m-%d').dt.date
-print(df.head(5))
-print(df.dtypes)
 helpers.write_snowflake(connection, df, table2)
 
 spark.stop()
